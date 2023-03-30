e96fe48c098206fcedcedcbb8b514ae21525968c
MerlinAlex <merlinalex@qq.com>
2023-03-30 17:08:52 +0800

    chore: init

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup>
+const darkMode = ref(false)
+</script>
+
+<template>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
